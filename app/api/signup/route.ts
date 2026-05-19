990a0d8bb782cf7f1939c21c921d41afd8470c86
ABHIII777 <noob.ssooff@gmail.com>
2026-05-19 14:10:51 +0530

    fix: add env validation using zod and set cookies on signup

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,8 +1,10 @@
 import bcrypt from "bcrypt";
-import {db} from "@/db";
-import {users} from "@/db/schema"
+import { db } from "@/db";
+import { users } from "@/db/schema"
 import { NextResponse } from "next/server"
 import { loginSchema, signupSchema } from "@/lib/validators";
+import { cookies } from "next/headers";
+import { signToken } from "@/lib/auth";
 
 export async function POST(req: Request) {
 
@@ -10,8 +12,8 @@ export async function POST(req: Request) {
 
     const parsed = signupSchema.safeParse(body);
 
-    if(!parsed.success) {
-        return NextResponse.json({error: parsed.error}, { status: 400});
+    if (!parsed.success) {
+        return NextResponse.json({ error: parsed.error }, { status: 400 });
     }
 
     const { email, password, name } = parsed.data;
@@ -19,14 +21,22 @@ export async function POST(req: Request) {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
-        await db.insert(users).values({
+        const user = await db.insert(users).values({
             email, password: hashedPassword, name
+        }).returning()
+
+        const token = signToken({ userId: user[0].id });
+
+        (await cookies()).set("token", token, {
+            httpOnly: true,
+            secure: true,
+            path: "/"
         })
 
-        return NextResponse.json({message: "User Created"})
+        return NextResponse.json({ message: "User Created" })
 
     } catch (err) {
-        return NextResponse.json({message: "User Already Exist"}, {status: 400});
+        return NextResponse.json({ message: "User Already Exist" }, { status: 400 });
     }
 
 }
---
+++ lib/env.ts
@@ -0,0 +1,6 @@
+import z from "zod"
+
+export const env = z.object({
+    DATABASE_URL: z.string(),
+    JWT_SECRET: z.string()
+}).parse(process.env)
